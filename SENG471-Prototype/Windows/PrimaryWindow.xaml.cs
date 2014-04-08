45a5ee28acd961e66d4a508379f2e7958cda0c7c
Daniel Sabourin <sabourid@ucalgary.ca>
2014-04-08 13:47:07 -0600

    Added transition to another window

---
+++ SENG471-Prototype/Windows/PrimaryWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SENG471_Prototype
+{
+    /// <summary>
+    /// Interaction logic for PrimaryWindow.xaml
+    /// </summary>
+    public partial class PrimaryWindow : UserControl
+    {
+        private string _userRole;
+        public string UserRole
+        {
+            get { return _userRole; }
+            set
+            {
+                _userRole = value;
+                TestLabel.Content = value;
+            }
+        }
+
+        public PrimaryWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
